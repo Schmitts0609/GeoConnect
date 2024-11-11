120ec67ba25776851b00081eb1007c3faf0052b9
Cauã <caua.schmitt@hotmail.com.br>
2024-11-11 00:42:13 -0300

    Atualização FrontEnd

---
+++ backEnd/BD.sql
@@ -0,0 +1,62 @@
+create database GeoConnect;
+use GeoConnect;
+
+CREATE TABLE cupons (
+    id int primary key auto_increment,
+    estabelecimento_id int,
+    descricao varchar(255),
+    valor_desconto decimal(10, 2),
+    pontos_necessarios int
+)
+
+CREATE TABLE estabelecimentos (
+    id int primary key auto_increment,
+    nome varchar(255) not null,
+    email varchar(255) not null,
+    cnpj varchar(255) not null,
+    codigo_validador_atual int,
+    CHECK (codigo_validador_atual BETWEEN 1000000 AND 9999999),
+    ultimo_codigo_validador int,
+    CHECK (ultimo_codigo_validador BETWEEN 1000000 AND 9999999),
+    hora_modificacao timestamp
+)
+
+CREATE TABLE historico_login (
+    id int auto_increment,
+    UserId int,
+    data_login varchar(255)
+)
+
+CREATE TABLE postagens (
+    id int primary key auto_increment
+    UserId int,
+    NomeImagem varchar(255),
+    ImagemPerfil varchar(255),
+    Tipo_postagem varchar(255),
+    Hora_postagem timestamp,
+    Legenda varchar(255)
+)
+
+CREATE TABLE resgate_cupom (
+    id int primary key auto_increment,
+    UserId int,
+    CupomId int,
+    Data_resgate timestamp,
+    Data_utilizado varchar(255)
+)
+
+CREATE TABLE seguindo (
+    UserId int primary key,
+    SeguindoId,
+    Data_seguindo timestamp
+)
+
+CREATE TABLE usuarios (
+    id int primary key auto_increment,
+    Nome varchar(255) not null,
+    Nickname varchar(18) not null,
+    Email varchar(254) not null,
+    Senha varchar(255) not null,
+    Data_criacao timestamp,
+    ImagemPerfil varchar(255)
+)
